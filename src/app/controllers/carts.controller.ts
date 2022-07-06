fedf91d2ed444c10668a4a272f1b2103f7f482e9
Luis Mera <luismera86@gmail.com>
2022-07-05 22:41:38 -0300

    Carts controller funcionando

--- src/app/controllers/carts.controller.ts
+++ src/app/controllers/carts.controller.ts
@@ -1,37 +1,33 @@
 import { Cart, Product } from '../../interfaces'
 import { Request, Response } from 'express'
-import { addProduct, getProducts, products } from './products.controller'
 
 import { Container } from '../models/container'
+import { products } from './products.controller'
 
 const saveCartToJson = new Container()
 
 let carts: Cart[] = []
 
 const insertCarts = () => {
-
-	
-
-	saveCartToJson.readCarts().then(resp => {		
-
-			carts = resp
-			if ( carts === undefined) {
-				let i = Date.now()
-				let newId = i * 3
-				carts = [{
+	saveCartToJson.readCarts().then(resp => {
+		carts = resp
+		if (carts === undefined) {
+			let i = Date.now()
+			let newId = i * 3
+			carts = [
+				{
 					id: newId,
 					timestamp: Date.now(),
-					products: []
-				}]
-			}
+					products: [],
+				},
+			]
+		}
 
 		console.log(carts)
 	})
 }
 
 insertCarts()
-
-
 
 const createCart = (req: Request, resp: Response) => {
 	try {
@@ -43,17 +39,14 @@ const createCart = (req: Request, resp: Response) => {
 			products: [],
 		}
 
-		
-			carts.push(newCart)
-		
-		
+		carts.push(newCart)
 
 		saveCartToJson.addCart(carts)
-		
 
 		console.log(carts)
 
 		resp.status(201).send('Carrito creado con éxito')
+		
 	} catch (error) {
 		console.log(`Lo sentimos hubo un error ${error}`)
 	}
@@ -80,13 +73,11 @@ const deleteCart = (req: Request, resp: Response) => {
 const getCartProducts = (req: Request, resp: Response) => {
 	try {
 		const idCartParams = Number(req.params.id)
-		const cartProducts = carts.find(c => {
-			if (c.id === idCartParams) {
-				return c.products
-			}
-		})
+		const cartProducts = carts.filter( c =>  c.id === idCartParams )
 
-		resp.status(200).send(cartProducts)
+
+
+		resp.status(200).send(cartProducts[0].products)
 	} catch (error) {
 		console.log(`Lo sentimos hubo un error ${error}`)
 	}
@@ -96,34 +87,54 @@ const addProductIdCart = async (req: Request, resp: Response) => {
 	try {
 		const idCartParams = Number(req.params.id) // Parámetro que recibimos del carrito
 		const idProduct = Number(req.body.id) // Id del producto obtenido por postman
-
+		const productIndex = products.findIndex( p => p.id === idProduct)
 		const productAdd: Product = products.find(p => p.id === idProduct)! // Obtenemos el producto
 		console.log(productAdd)
-	
 		const cartIndex = carts.findIndex(cart => cart.id === idCartParams) // Obtenemos el indice del array donde está el carrito
 
-		carts[cartIndex].products.push(productAdd)
+		if ( products[productIndex] ) {
+			
+			carts[cartIndex].products.push(productAdd)
 
-		console.log('Este son los carts después de cargar el producto')
-		console.log(carts)
+			saveCartToJson.addCart(carts)
+			resp.status(201).send('Producto agregado al carrito con éxito')
+		} else {
 
-		resp.status(201).send('Producto agregado al carrito con éxito')
+			resp.status(400).send('No se pudo encontrar el producto')
+		}
+
+			
+
 	} catch (error) {
 		console.log(`Lo sentimos hubo un error ${error}`)
 	}
 }
 
 const deleteProductCart = (req: Request, resp: Response) => {
+	const idCartParams = Number(req.params.id) // Parámetro que recibimos del carrito
+	const idProduct = Number(req.params.id_prod) // Id del producto obtenido por postman
+	const cartIndex = carts.findIndex(cart => cart.id === idCartParams)
+	const productIndexCart = carts[cartIndex].products.findIndex( p => p.id === idProduct )
+	console.log(productIndexCart)
 
-		const idCartParams = Number(req.params.id) // Parámetro que recibimos del carrito
-		const idProduct = Number(req.params.id) // Id del producto obtenido por postman
-		const cartIndex = carts.findIndex(cart => cart.id === idCartParams)
-		const productIndexCart = carts[cartIndex].products.findIndex( p => p.id === idProduct) // Obtenemos el indice del array de productos del carrito 
-		carts[cartIndex].products.slice(productIndexCart, 1)
-		
+	if ( carts[cartIndex].products[productIndexCart]) {
+
+		  carts[cartIndex].products.splice(productIndexCart, 1)
 
 
+		saveCartToJson.addCart(carts)
 
+
+		resp.status(200).send('Producto del carrito eliminado con éxito')
+
+	} else {
+
+		resp.status(404).send('Lo sentimos no pudimos encontrar el producto')
+	}
+	
+	
+	
+	
 }
 
 export {
